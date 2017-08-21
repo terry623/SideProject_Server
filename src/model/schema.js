86b53ac1ec469d15968f4db41e574cfe085a3f42
terry623 <terry92516@gmail.com>
2017-08-21 23:08:04 +0800

    add new schema

--- src/model/schema.js
+++ src/model/schema.js
@@ -3,16 +3,20 @@ const pgp = require('pg-promise')();
 const db = pgp(process.env.DB_URL);
 
 const schemaSql = `
-    -- Drop (droppable only when no dependency)
     DROP TABLE IF EXISTS Users;
 
-    -- Create
     CREATE TABLE Users (
         id              serial PRIMARY KEY NOT NULL,
         "username"      varchar(50) NOT NULL,
         "password"      varchar(50) NOT NULL,
         "store_lat"     float NOT NULL DEFAULT 0,
         "store_lng"     float NOT NULL DEFAULT 0
+    );
+
+    CREATE TABLE Photos (
+        id              serial PRIMARY KEY NOT NULL,
+        "username"      varchar(50) NOT NULL,
+        "url"           varchar(100) NOT NULL
     );
 `;
 
