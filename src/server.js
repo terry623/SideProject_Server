79caf8cfd12778811e65eb1d5c3b269b15127f90
terry623 <terry92516@gmail.com>
2017-08-26 17:43:52 +0800

    initial chat server

--- src/server.js
+++ src/server.js
@@ -1,23 +1,51 @@
 require('../config.js');
-const express = require('express');
+// const express = require('express');
 
-const all_router = require('./routers/all_router.js');
-const requestLogger = require('./middleware/request-logger.js');
-const errorHandler = require('./middleware/error-handler.js');
+// const all_router = require('./routers/all_router.js');
+// const requestLogger = require('./middleware/request-logger.js');
+// const errorHandler = require('./middleware/error-handler.js');
+// const app = express();
 
-const app = express();
+// var port = 8080;
+// var server = app.listen(port, () => {
+//     console.log(`Server is up and running on port ${port}...`);
+// });
+// var io = require('socket.io').listen(server);
 
-// app.use(requestLogger);
-app.use(express.static('dist', {
-    setHeaders: (res, path, stat) => {
-        res.set('Cache-Control', 'public, s-maxage=86400');
-    }
-}));
-app.use('/api', all_router);
-app.get('/*', (req, res) => res.redirect('/'));
-app.use(errorHandler);
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
 
-const port = 8080;
-app.listen(port, () => {
-    console.log(`Server is up and running on port ${port}...`);
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(80);
+
+// app.use(express.static('dist'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
+
+// app.use('/api', all_router);
+// app.get('/*', (req, res) => res.redirect('/'));
+// app.use(errorHandler);
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+      console.log(data);
+    });
+  });
+
+// io.on('connection', function (socket) {
+//     socket.on('chat message', function (msg) {
+//         console.log("message: ", msg);
+//         io.emit('chat message', msg);
+//     });
+// });
+
+// http.listen(port, function () {
+//     console.log('listening on *:' + port);
+// });
