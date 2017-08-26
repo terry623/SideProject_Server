0db19fedf594598874fd8b552fcc6eeb883b556a
terry623 <terry92516@gmail.com>
2017-08-26 20:12:49 +0800

    finish simple connection

--- src/server.js
+++ src/server.js
@@ -1,51 +1,29 @@
 require('../config.js');
-// const express = require('express');
+var express = require('express');
+var all_router = require('./routers/all_router.js');
+var requestLogger = require('./middleware/request-logger.js');
+var errorHandler = require('./middleware/error-handler.js');
+var app = express();
 
-// const all_router = require('./routers/all_router.js');
-// const requestLogger = require('./middleware/request-logger.js');
-// const errorHandler = require('./middleware/error-handler.js');
-// const app = express();
-
-// var port = 8080;
-// var server = app.listen(port, () => {
-//     console.log(`Server is up and running on port ${port}...`);
-// });
-// var io = require('socket.io').listen(server);
-
-// var app = require('express')();
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-
-server.listen(80);
-
-// app.use(express.static('dist'));
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+var port = 8080;
+var server = app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}...`);
 });
+var io = require('socket.io').listen(server);
 
-// app.use('/api', all_router);
-// app.get('/*', (req, res) => res.redirect('/'));
-// app.use(errorHandler);
+app.use(express.static('dist'));
+app.use('/api', all_router);
+app.get('/chat', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/*', (req, res) => res.redirect('/'));
+app.use(errorHandler);
 
 io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-      console.log(data);
-    });
+  socket.emit('news', {
+    hello: 'world'
   });
-
-// io.on('connection', function (socket) {
-//     socket.on('chat message', function (msg) {
-//         console.log("message: ", msg);
-//         io.emit('chat message', msg);
-//     });
-// });
-
-// http.listen(port, function () {
-//     console.log('listening on *:' + port);
-// });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
