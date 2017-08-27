1a39d7b66c3c44615b53e44fb29ae7f55dcc8c21
terry623 <terry92516@gmail.com>
2017-08-27 23:03:35 +0800

    cannot send msg by id

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ var app = express();
 
 var port = 8080;
 var server = app.listen(port, () => {
-    console.log(`Server is up and running on port ${port}...`);
+  console.log(`Server is up and running on port ${port}...`);
 });
 var io = require('socket.io').listen(server);
 
@@ -16,9 +16,16 @@ app.use('/api', all_router);
 app.get('/*', (req, res) => res.redirect('/'));
 app.use(errorHandler);
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log(msg);
-    io.emit('chat message', msg);
+io.on('connection', function (socket) {
+
+  io.clients((error, clients) => {
+    if (error) throw error;
+    console.log(clients);
   });
+
+  socket.on('chat message', function (id, msg) {
+    console.log("id: " + id + " , msg: " + msg);
+    socket.broadcast.to(id).emit('my message', msg);
+  });
+
 });
